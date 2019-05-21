0cd8c76aa8805ed8928e6cb98cddceff98bdd20e
Steve Burkett <steve@burkettnet.com>
2019-05-21 13:23:49 -0700

    Memoize a book part's pages

--- lib/openstax/cnx/v1/book_part.rb
+++ lib/openstax/cnx/v1/book_part.rb
@@ -1,13 +1,15 @@
 module OpenStax::Cnx::V1
   class BookPart
 
+    CONTENTS = 'contents'
+
+    attr_reader :hash, :is_root, :book
+
     def initialize(hash: {}, is_root: false, book: nil)
       @hash = hash
       @is_root = is_root
       @book = book
     end
-
-    attr_reader :hash, :is_root, :book
 
     def parsed_title
       @parsed_title ||= OpenStax::Cnx::V1::Baked.parse_title(
@@ -24,14 +26,14 @@ module OpenStax::Cnx::V1
     end
 
     def contents
-      @contents ||= hash.fetch('contents') do |key|
+      @contents ||= hash.fetch(CONTENTS) do |key|
         raise "#{self.class.name} id=#{@id} is missing #{key}"
       end
     end
 
     def parts
       @parts ||= contents.map do |hash|
-        if hash.has_key? 'contents'
+        if hash.has_key? CONTENTS
           self.class.new(hash: hash, book: book)
         else
           OpenStax::Cnx::V1::Page.new(hash: hash, book: book)
@@ -45,7 +47,7 @@ module OpenStax::Cnx::V1
     end
 
     def pages
-      parts.map do |part|
+      @pages ||= parts.map do |part|
         part.is_a?(Page) ? part : part.pages
       end.flatten
     end
